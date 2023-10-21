b546d66de5a24d8ed56e9150f6c81fac66f7babc
Johnny <johnny@Eric---2.local>
2023-10-21 20:39:37 +0800

    sucessful connection between SQL server (Note that sqlite-jdbc-3.32.3.2 is used, which is not the most recent version. Something is wrong with the newest one for future note

--- src/model/Test.java
+++ src/model/Test.java
@@ -1,13 +1,46 @@
 package model;
-import java.time.format.DateTimeFormatter;  
+import java.time.format.DateTimeFormatter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.time.LocalDateTime;    
 
 public class Test{
-	 public static void main(String[] args) {    
-//		   DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
-		   DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm");  
-		   LocalDateTime now = LocalDateTime.now();  
-		   System.out.println(dtf.format(now));  
-		  }    
+//	 public static void main(String[] args) {    
+////		   DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
+//		   DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm");  
+//		   LocalDateTime now = LocalDateTime.now();  
+//		   System.out.println(dtf.format(now));  
+//		  }    
+	
+	public static void connect() {  
+        Connection conn = null;  
+        try {  
+            // db parameters  
+            String url = "jdbc:sqlite:/Users/johnny/git/PoliceApp/src/model/userInfoSQL.db";  
+            // create a connection to the database  
+            conn = DriverManager.getConnection(url);  
+              
+            System.out.println("Connection to SQLite has been established.");  
+              
+        } catch (SQLException e) {  
+            System.out.println(e.getMessage());  
+        } finally {  
+            try {  
+                if (conn != null) {  
+                    conn.close();  
+                }  
+            } catch (SQLException ex) {  
+                System.out.println(ex.getMessage());  
+            }  
+        }  
+    } 
+ 
+    /** 
+     * @param args the command line arguments 
+     */  
+ public static void main(String[] args) {  
+        connect();  
+  }  
 
 }
--- src/model/UserManager.java
+++ src/model/UserManager.java
@@ -1,6 +1,9 @@
 package model;
 
 import java.util.LinkedList;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 
 public class UserManager {
@@ -38,4 +41,33 @@ public class UserManager {
 		}
 		return check;
 	}
+	public static void connect() {  
+        Connection conn = null;  
+        try {  
+            // db parameters  
+            String url = "jdbc:sqlite:/Users/johnny/git/PoliceApp/src/model/userInfoSQL.db";  
+            // create a connection to the database  
+            conn = DriverManager.getConnection(url);  
+               
+            System.out.println("Connection to SQLite has been established.");  
+              
+        } catch (SQLException e) {  
+            System.out.println(e.getMessage());  
+        } finally {  
+            try {  
+                if (conn != null) {  
+                    conn.close();  
+                }  
+            } catch (SQLException ex) {  
+                System.out.println(ex.getMessage());  
+            }  
+        }  
+    } 
+ 
+    /** 
+     * @param args the command line arguments 
+     */  
+ public static void main(String[] args) {  
+        connect();  
+  }  
 }
